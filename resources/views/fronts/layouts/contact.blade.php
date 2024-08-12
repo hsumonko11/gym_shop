a543ba44d91a302512909ea6f6e8468b271f2da6
hsumonko11 <hsumonko139@gmail.com>
2024-08-12 10:57:43 +0630

    monday

--- resources/views/fronts/layouts/contact.blade.php
+++ resources/views/fronts/layouts/contact.blade.php
@@ -21,20 +21,20 @@
                 <form action="{{route('contact.store')}}" method="POST">
                     @csrf
                     <h4 class="mtext-105 cl2 txt-center p-b-30">
-                        Send Us A Message
+                        အကြံပြုစာပေးပို့ရန်
                     </h4>
 
                     <div class="bor8 m-b-20 how-pos4-parent">
-                        <input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="email" placeholder="Your Email Address">
+                        <input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="email" placeholder="သင့်အား ဆက်သွယ်လိုသော အီးမေးလ်ကိုသာ ထည့်သွင်းပေးပါရှင်">
                         <img class="how-pos4 pointer-none" src="{{asset('fronts/images/icons/icon-email.png')}}" alt="ICON">
                     </div>
 
                     <div class="bor8 m-b-30">
-                        <textarea class="stext-111 cl2 plh3 size-120 p-lr-28 p-tb-25" name="note" placeholder="How Can We Help?"></textarea>
+                        <textarea class="stext-111 cl2 plh3 size-120 p-lr-28 p-tb-25" name="note" placeholder="အကြောင်းအရာ . . .?"></textarea>
                     </div>
 
                     <button type="submit" class="flex-c-m stext-101 cl0 size-121 bg3 bor1 hov-btn3 p-lr-15 trans-04 pointer">
-                        Submit
+                        ပေးပို့ရန်
                     </button>
                 </form>
             </div>

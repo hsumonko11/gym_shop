609f73d74efe7beca88d3760cec93ff21e920d86
hsumonko11 <hsumonko139@gmail.com>
2024-08-18 22:26:27 +0630

    Sunday

--- resources/views/fronts/shops/cart.blade.php
+++ resources/views/fronts/shops/cart.blade.php
@@ -3,121 +3,121 @@
 @section('shop-active','active-menu')
 
 @section('content')
-	<!-- Cart -->
-	<div class="wrap-header-cart js-panel-cart">
-		<div class="s-full js-hide-cart"></div>
+     <!-- Cart -->
+     <div class="wrap-header-cart js-panel-cart">
+          <div class="s-full js-hide-cart"></div>
 
-		<div class="header-cart flex-col-l p-l-65 p-r-25">
-			<div class="header-cart-title flex-w flex-sb-m p-b-8">
-				<span class="mtext-103 cl2">
-					Your Cart
-				</span>
+          <div class="header-cart flex-col-l p-l-65 p-r-25">
+               <div class="header-cart-title flex-w flex-sb-m p-b-8">
+                    <span class="mtext-103 cl2">
+                         Your Cart
+                    </span>
 
-				<div class="fs-35 lh-10 cl2 p-lr-5 pointer hov-cl1 trans-04 js-hide-cart">
-					<i class="zmdi zmdi-close"></i>
-				</div>
-			</div>
+                    <div class="fs-35 lh-10 cl2 p-lr-5 pointer hov-cl1 trans-04 js-hide-cart">
+                         <i class="zmdi zmdi-close"></i>
+                    </div>
+               </div>
 
-			<div class="header-cart-content flex-w js-pscroll">
-				<ul class="header-cart-wrapitem w-full">
-					<li class="header-cart-item flex-w flex-t m-b-12">
-						<div class="header-cart-item-img">
-							<img src="images/item-cart-01.jpg" alt="IMG">
-						</div>
+               <div class="header-cart-content flex-w js-pscroll">
+                    <ul class="header-cart-wrapitem w-full">
+                         <li class="header-cart-item flex-w flex-t m-b-12">
+                              <div class="header-cart-item-img">
+                                   <img src="images/item-cart-01.jpg" alt="IMG">
+                              </div>
 
-						<div class="header-cart-item-txt p-t-8">
-							<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-								White Shirt Pleat
-							</a>
+                              <div class="header-cart-item-txt p-t-8">
+                                   <a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
+                                        White Shirt Pleat
+                                   </a>
 
-							<span class="header-cart-item-info">
-								1 x $19.00
-							</span>
-						</div>
-					</li>
+                                   <span class="header-cart-item-info">
+                                        1 x $19.00
+                                   </span>
+                              </div>
+                         </li>
 
-					<li class="header-cart-item flex-w flex-t m-b-12">
-						<div class="header-cart-item-img">
-							<img src="images/item-cart-02.jpg" alt="IMG">
-						</div>
+                         <li class="header-cart-item flex-w flex-t m-b-12">
+                              <div class="header-cart-item-img">
+                                   <img src="images/item-cart-02.jpg" alt="IMG">
+                              </div>
 
-						<div class="header-cart-item-txt p-t-8">
-							<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-								Converse All Star
-							</a>
+                              <div class="header-cart-item-txt p-t-8">
+                                   <a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
+                                        Converse All Star
+                                   </a>
 
-							<span class="header-cart-item-info">
-								1 x $39.00
-							</span>
-						</div>
-					</li>
+                                   <span class="header-cart-item-info">
+                                        1 x $39.00
+                                   </span>
+                              </div>
+                         </li>
 
-					<li class="header-cart-item flex-w flex-t m-b-12">
-						<div class="header-cart-item-img">
-							<img src="images/item-cart-03.jpg" alt="IMG">
-						</div>
+                         <li class="header-cart-item flex-w flex-t m-b-12">
+                              <div class="header-cart-item-img">
+                                   <img src="images/item-cart-03.jpg" alt="IMG">
+                              </div>
 
-						<div class="header-cart-item-txt p-t-8">
-							<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-								Nixon Porter Leather
-							</a>
+                              <div class="header-cart-item-txt p-t-8">
+                                   <a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
+                                        Nixon Porter Leather
+                                   </a>
 
-							<span class="header-cart-item-info">
-								1 x $17.00
-							</span>
-						</div>
-					</li>
-				</ul>
+                                   <span class="header-cart-item-info">
+                                        1 x $17.00
+                                   </span>
+                              </div>
+                         </li>
+                    </ul>
 
-				<div class="w-full">
-					<div class="header-cart-total w-full p-tb-40">
-						Total: $75.00
-					</div>
+                    <div class="w-full">
+                         <div class="header-cart-total w-full p-tb-40">
+                              Total: $75.00
+                         </div>
 
-					<div class="header-cart-buttons flex-w w-full">
-						<a href="shoping-cart.html" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
-							View Cart
-						</a>
+                         <div class="header-cart-buttons flex-w w-full">
+                              <a href="shoping-cart.html" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
+                                   View Cart
+                              </a>
 
-						<a href="shoping-cart.html" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-b-10">
-							Check Out
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-
-	<!-- breadcrumb -->
-	<div class="container p-t-75">
-		<div class="bread-crumb flex-w p-l-25 p-r-15 p-t-30 p-lr-0-lg">
-			<a href="index.html" class="stext-109 cl8 hov-cl1 trans-04">
-				Home
-				<i class="fa fa-angle-right m-l-9 m-r-10" aria-hidden="true"></i>
-			</a>
-
-			<span class="stext-109 cl4">
-				Shoping Cart
-			</span>
-		</div>
-	</div>
+                              <a href="shoping-cart.html" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-b-10">
+                                   Check Out
+                              </a>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
 
 
-	<!-- Shoping Cart ဈေးခြင်းတောင်း
+     <!-- breadcrumb -->
+     <div class="container p-t-75">
+          <div class="bread-crumb flex-w p-l-25 p-r-15 p-t-30 p-lr-0-lg">
+               <a href="index.html" class="stext-109 cl8 hov-cl1 trans-04">
+                    Home
+                    <i class="fa fa-angle-right m-l-9 m-r-10" aria-hidden="true"></i>
+               </a>
+
+               <span class="stext-109 cl4">
+                    Shoping Cart
+               </span>
+          </div>
+     </div>
+
+
+     <!-- Shoping Cart ဈေးခြင်းတောင်း
 ကုန်ပစ္စည်း ဈေးနှုန်း အရေအတွက် ကျသင့်ငွေ လုပ်ဆောင်ချက်။ စုစုပေါင်းကျသင့်ငွေ
 အချက်အလက်များကို ပြည့်စုံစွာ ဖြည့်ပေးပါရှင့်
 ဘောက်ချာနံပါတ် အမည် ဖုန်း နေရပ်လိပ်စာ(အိမ်အမှတ် ၊ လမ်းအမည် ၊မြို့နယ် )
 ငွေပေးချေပြီးနောက် ပြေစာကိုထည့်သွင်းပေးပါ
 အတည်ပြုမည်-->
-	<form class="bg0 p-t-25 p-b-85">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-10 col-xl-7 m-lr-auto m-b-50">
-					<div class="m-l-25 m-r--38 m-lr-0-xl">
-						<div class="wrap-table-shopping-cart card-shadow">
-							<table class="table-shopping-cart">
-								<thead>
+     <form class="bg0 p-t-25 p-b-85">
+          <div class="container">
+               <div class="row">
+                    <div class="col-lg-10 col-xl-7 m-lr-auto m-b-50">
+                         <div class="m-l-25 m-r--38 m-lr-0-xl">
+                              <div class="wrap-table-shopping-cart card-shadow">
+                                   <table class="table-shopping-cart">
+                                        <thead>
                                     <tr class="table_head">
                                         <th class="column-1" style="font-size: 20px;">ကုန်ပစ္စည်း</th>
                                         <th class="column-2" style="font-size: 20px;"></th>
@@ -129,7 +129,7 @@
                                 </thead>
 
 
-								<tbody>
+                                        <tbody>
                                     @foreach($carts as $cart)
                                     <tr class="table_row">
                                         <td class="column-1">
@@ -138,7 +138,7 @@
                                         <td class="column-2">{{$cart->product->name}}</td>
                                         <td class="column-3 price">{{$cart->product->price}} MMK</td>
                                         <td class="column-4 quantity">{{$cart->quantity}}
-                                            {{-- <div class="wrap-num-product flex-w m-l-auto m-r-0">
+                                            <!-- <div class="wrap-num-product flex-w m-l-auto m-r-0">
                                                 <div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m quantity-sign">
                                                     <i class="fs-16 zmdi zmdi-minus"></i>
                                                 </div>
@@ -150,12 +150,13 @@
                                                 </div>
 
 
-                                            </div> --}}
+                                            </div> -->
                                         </td>
                                         <td class="column-5 total">{{$cart->product->price * $cart->quantity}}</td>
                                         <td>
                                             <button type="button" class="btn btn-md btn-outline-danger remove-cart" data-route="{{route('remove_cart',$cart->id)}}"><i class="fa fa-trash"></i></button>
                                             <input type="hidden" name="cart_id" class="cart-id" value="{{$cart->id}}">
+                                            <input type="hidden" name="quantity" class="cart-quantity" value="{{$cart->quantity}}">
                                             <input type="hidden" name="product_id" class="product-id" value="{{$cart->product_id}}">
                                             <input type="hidden" name="user_id" class="user-id" value="{{auth()->user()->id}}">
                                         </td>
@@ -164,50 +165,50 @@
                                 </tbody>
 
 
-							</table>
-						</div>
+                                   </table>
+                              </div>
 
-					</div>
-				</div>
+                         </div>
+                    </div>
 
-				<div class="col-sm-10 col-lg-7 col-xl-5 m-lr-auto m-b-50">
-					<div class="bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-r-40 m-lr-0-xl p-lr-15-sm card-shadow">
-						<h4 class="mtext-109 cl2 p-b-30">
-							ဈေးခြင်းတောင်း
-						</h4>
+                    <div class="col-sm-10 col-lg-7 col-xl-5 m-lr-auto m-b-50">
+                         <div class="bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-r-40 m-lr-0-xl p-lr-15-sm card-shadow">
+                              <h4 class="mtext-109 cl2 p-b-30">
+                                   ဈေးခြင်းတောင်း
+                              </h4>
 
-						<div class="flex-w flex-t bor12 p-b-13">
-							<div class="size-208">
-								<span class="stext-110 cl2" style="font-size: 15px;">
-									စုစုပေါင်းကျသင့်ငွေ =
-								</span>
-							</div>
+                              <div class="flex-w flex-t bor12 p-b-13">
+                                   <div class="size-208">
+                                        <span class="stext-110 cl2" style="font-size: 15px;">
+                                             စုစုပေါင်းကျသင့်ငွေ =
+                                        </span>
+                                   </div>
 
-							<div class="size-209">
-								<span class="mtext-110 cl2 grand-total">
+                                   <div class="size-209">
+                                        <span class="mtext-110 cl2 grand-total">
 
-								</span>
-							</div>
-						</div>
-
-
-
-						<button class="flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer checkout" >
-							မှာယူမည်
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	</form>
+                                        </span>
+                                   </div>
+                              </div>
 
 
-	<!-- Back to top -->
-	<div class="btn-back-to-top" id="myBtn">
-		<span class="symbol-btn-back-to-top">
-			<i class="zmdi zmdi-chevron-up"></i>
-		</span>
-	</div>
+
+                              <button class="flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer checkout" >
+                                   မှာယူမည်
+                              </button>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </form>
+
+
+     <!-- Back to top -->
+     <div class="btn-back-to-top" id="myBtn">
+          <span class="symbol-btn-back-to-top">
+               <i class="zmdi zmdi-chevron-up"></i>
+          </span>
+     </div>
 
 @endsection
 
@@ -282,7 +283,7 @@
 
                $('.table-shopping-cart tbody tr').each(function(index,row){
                     $productId = $(row).find('.product-id').val();
-                    $quantity = $(row).find('.quantity').val();
+                    $quantity = $(row).find('.cart-quantity').val();
                     $cartId = $(row).find('.cart-id').val();
                     $total = $(row).find('.total').text();
                     $orderList.push({
